b74ee5bf224b97ce94f14c4d6fcf44d18bbedb88
Abhishek singh <abhisheks.067@gmail.com>
2025-11-03 15:30:25 +0530

    adding api data and add to cart function

---
+++ BOOK-VITE-APP/BOOK-VITE-APP/src/component/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <h3>Design and Developed By Abhishek Singh
+      </h3>
+    </div>
+  )
+}
+
+export default Footer
