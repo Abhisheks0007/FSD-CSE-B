eae9d5aa58fc76148658406d0dfccb248cf588d7
Abhishek singh <abhisheks.067@gmail.com>
2026-02-19 14:37:48 +0530

    hostname

--- nodejs/index.js
+++ nodejs/index.js
@@ -79,3 +79,4 @@ console.log("free memory",os.freemem());
 console.log("total memory",os.totalmem());
 console.log("uptime",os.uptime());
 console.log("home dir",os.homedir());
+console.log("hostname",os.hostname());
